019231ee43630a76011cf42cd1888ac2e998cd96
Pralhad P. Teggi <pralhad2481@gmail.com>
2024-04-27 23:04:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -36,8 +36,11 @@ max = users.shape[0] -1
 recomm = None
 def home_page():
     st.write(f"""# Recommendation System""", unsafe_allow_html=True)
-    
-    
+
+    st.write(f"""#### Study Material Recommendation Systems based on Study Patterns and Cognitive learning""", unsafe_allow_html=True)
+    st.write(f"""<p>This project takes the log files of user as initial input and extracts their study patterns based on their online activity. The recommendation 
+    generator takes those features as input, tries to find similar users based comparing the study patterns and recommends courses they completed.
+    </p>""", unsafe_allow_html=True)
     input_userid = st.number_input(label="Insert an index between 0 to {{max}} to choose a USER ID", min_value=0, max_value=100, step=1, key="test_slider1")
     n = st.number_input(label="Number of Recommendations", min_value=0, max_value=100, step=1, key="test_slider2")
     
