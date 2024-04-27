20c73069225262ec621c4a66b7dbbb7fe7e96fd9
Pralhad P. Teggi <pralhad2481@gmail.com>
2024-04-27 22:56:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -52,9 +52,9 @@ def home_page():
 	    #Running the model, generating recommendations and passing the list to the HTML page
 	    model = recommendationGenerator(userID, n)
 	    recomm = model.generate_recommendations(features,data)
-            if recomm != None :
+	    if recomm != None :
             	st.write(f"""#### Recommendation System Results""", unsafe_allow_html=True)
-    	    	st.write(recomm)
+        	st.write(recomm)
 	    
 def results():
     st.write(f"""# Recommendation System Results""", unsafe_allow_html=True)
@@ -67,54 +67,6 @@ def about_page():
         <p>Vartur Lake, located in Bangalore, India, is a significant water body that plays a crucial role in the local ecosystem and urban landscape.</p> 
     """, unsafe_allow_html=True)
     
-
-
-def model_section():
-    st.write("""<h1>Predict Water Quality</h1>
-    <p>Enter these values of the parameters to know if the water quality is suitable to drink or not.</p><hr>
-    """, unsafe_allow_html=True)
-
-    col1, col2, col3 = st.columns(3, gap="large")
-    with col1:
-        TurbidityNTU = st.number_input(label="Turbidity (NTU)", min_value=0.0, max_value=1000.0, step=50.0, format="%f",
-                                       key="test_slider1")
-        pH = st.number_input(label="pH", min_value=0.0, max_value=1000.0, step=50.0, format="%f", key="test_slider2")
-        
-        ConductivityuS = st.number_input(label="Conductivity (uS/cm)", min_value=0.0, max_value=1000.0, step=50.0,
-                                         format="%f", key="test_slider3")
-    with col2:
-        BOD = st.number_input(label="BOD (mg/l)", min_value=0.0, max_value=1000.0, step=50.0, format="%f",key="test_slider4")
-        COD = st.number_input(label="COD (mg/l)", min_value=0.0, max_value=1000.0, step=50.0, format="%f",key="test_slider5")
-        DissolvedOxygen = st.number_input(label="Dissolved Oxygen (mg/l)", min_value=0.0, max_value=1000.0, step=50.0, format="%f",key="test_slider6")
-        
-    with col3:
-        Chloride = st.number_input(label="Chloride (mg/l)", min_value=0.0, max_value=1000.0, step=50.0, format="%f",
-                                   key="test_slider7")
-        Nitrate = st.number_input(label="Nitrate (mg/l)", min_value=0.0, max_value=1000.0, step=50.0, format="%f",
-                                  key="test_slider8")
-                                  
-        st.write("<br>", unsafe_allow_html=True)
-        
-    predict_button = st.button('  Predict Water Quality  ')
-
-    dataframe = pd.DataFrame({'Turbidity (NTU)': [TurbidityNTU], 'pH': [pH],
-                              'Conductivity (uS/cm)': [ConductivityuS],
-                              'COD (mg/l)': [COD],
-                              'BOD (mg/l)': [BOD], 'Dissolved Oxygen (mg/l)': [DissolvedOxygen],
-                              'Chloride (mg/l)': [Chloride], 'Nitrate (mg/l)': [Nitrate],
-                             })
-
-    if predict_button:
-        model = load_model()
-        result = model.predict(dataframe)
-        for _ in stqdm(range(50)):
-            sleep(0.015)
-        if result[0] == 1.0:
-            st.error("This Water Quality is Non-Potable")
-        else:
-            st.success('This Water Quality is Potable')
-
-
 
 
 with st.sidebar:
