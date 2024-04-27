92461154a3388904843580b517d0e0da2be036a6
Pralhad P. Teggi <pralhad2481@gmail.com>
2024-04-27 22:38:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -52,8 +52,9 @@ def home_page():
 	    #Running the model, generating recommendations and passing the list to the HTML page
 	    model = recommendationGenerator(userID, n)
 	    recomm = model.generate_recommendations(features,data)
-    st.write(f"""#### Recommendation System Results""", unsafe_allow_html=True)
-    st.write(recomm)
+    if recomm :
+    	st.write(f"""#### Recommendation System Results""", unsafe_allow_html=True)
+    	st.write(recomm)
 	    
 def results():
     st.write(f"""# Recommendation System Results""", unsafe_allow_html=True)
